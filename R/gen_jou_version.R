950ca575ce2da94029abfa29edcdd3ba8a47eb29
Aaron Peikert <aaron.peikert@posteo.de>
2019-09-04 10:58:08 +0200

    add note about wrapping closes #40

--- R/gen_jou_version.R
+++ R/gen_jou_version.R
@@ -9,4 +9,8 @@ pos_linewidth <- grep("knitr::opts_chunk$set(linewidth = Inf)",
                       doc,
                       fixed = TRUE)
 doc[pos_linewidth] <- "knitr::opts_chunk$set(linewidth = 40)"
+pos_authornote <- grep("^authornote", doc)
+doc[pos_authornote] <- paste(doc[pos_authornote],
+                             "Please note that code longer then 40 characters is continued on the next line and indended by four spaces.")
+
 write_lines(doc, here("rr-flow-jou.Rmd"))
